5ae5cabe2f98ef02a73a20f80f009d8b766a0814
Amin <amin81shams@gmail.com>
2023-12-13 23:18:29 +0330

    add color changing feature

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let body = document.addEventListener("click", ()=> {
+    body.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`
+})
+
+function randomColor() {
+    // pass
+}
